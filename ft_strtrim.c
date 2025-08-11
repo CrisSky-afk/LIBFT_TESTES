b7f445672a01ca85e2100fdbb22486782b351abb
Cristiane_Suominsky <cristianesuominsky204@gmail.com>
2025-08-10 23:03:08 -0300

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -14,23 +14,24 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t		start;
-	size_t		end;
-	char		*str;
+	int		start;
+	int		end;
+	char	*str;
 
 	if (!s1 || !set)
 		return (NULL);
 	start = 0;
-	while (s1[start] != '\0' && ft_strchr(set, s1[start]))
-		start++;
 	end = ft_strlen(s1) - 1;
-	while (end > start && ft_strchr(set, s1[end]))
+	while (ft_strchr(set, s1[start]) && start <= end)
+		start++;
+	if (start > end)
+		return (ft_strdup(s1 + end + 1));
+	while (ft_strchr(set, s1[end]) && end >= 0)
 		end--;
-	str = malloc((end - start + 1) + 1);
+	str = malloc(end - start + 2);
 	if (!str)
 		return (NULL);
-	ft_memcpy(str, s1 + start, end - start + 1);
-	str[end - start + 1] = '\0';
+	ft_strlcpy(str, &s1[start], end - start + 2);
 	return (str);
 }
 
